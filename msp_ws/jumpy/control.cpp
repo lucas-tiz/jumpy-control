17e22a42f7a57018eb733108f67f15ee2dd9ee24
lucas tiziani <ltiziani3@gatech.edu>
2020-10-18 19:44:53 -0400

    updated control state logic

--- msp_ws/jumpy/control.cpp
+++ msp_ws/jumpy/control.cpp
@@ -6,7 +6,7 @@ void controlUpdate(void) {
     // 3-way inflate/seal/vent valve control
 //    MAP_GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN5); //DEBUG
 
-    static float pres_deadband[4] = {1,1,1,1};
+    static float pres_deadband[4] = {15,15,15,15};
     static float pres_undershoot[4] = {0,0,0,0};
     int idx_valve; // index of actuator
     int idx_trans; // index of pressure transducer
@@ -15,22 +15,37 @@ void controlUpdate(void) {
 
 //        pres[idx_trans][0] = 0; //DEBUG
 
-        if (pres_des[idx_valve] < 0) { // seal (manual)
+        // seal (manual)
+        if (pres_des[idx_valve] < 0) {
             MAP_GPIO_setOutputLowOnPin(valves[idx_valve].port, valves[idx_valve].pin_vent);
             MAP_GPIO_setOutputLowOnPin(valves[idx_valve].port, valves[idx_valve].pin_inflate);
         }
-        else if (pres_des[idx_valve] == 0) { // vent
+
+        // vent (manual)
+        else if (pres_des[idx_valve] == 0) {
             MAP_GPIO_setOutputHighOnPin(valves[idx_valve].port, valves[idx_valve].pin_vent);
             MAP_GPIO_setOutputLowOnPin(valves[idx_valve].port, valves[idx_valve].pin_inflate);
         }
-        else if (pres[idx_trans][0] >= (pres_des[idx_valve]+pres_undershoot[idx_valve])) { // seal (feedback control)
-            MAP_GPIO_setOutputLowOnPin(valves[idx_valve].port, valves[idx_valve].pin_vent);
-            MAP_GPIO_setOutputLowOnPin(valves[idx_valve].port, valves[idx_valve].pin_inflate);
-        }
-        else if (pres[idx_trans][0] < (pres_des[idx_valve] - pres_deadband[idx_valve])) { // inflate
+
+        // inflate: pressure < setpoint - undershoot
+        else if (pres[idx_trans][0] < (pres_des[idx_valve] - pres_undershoot[idx_valve])) { // inflate
             MAP_GPIO_setOutputLowOnPin(valves[idx_valve].port, valves[idx_valve].pin_vent);
             MAP_GPIO_setOutputHighOnPin(valves[idx_valve].port, valves[idx_valve].pin_inflate);
         }
+
+        // seal (feedback control): pressure <= setpoint + deadband
+        else if (pres[idx_trans][0] <= (pres_des[idx_valve] + pres_deadband[idx_valve])) {
+            MAP_GPIO_setOutputLowOnPin(valves[idx_valve].port, valves[idx_valve].pin_vent);
+            MAP_GPIO_setOutputLowOnPin(valves[idx_valve].port, valves[idx_valve].pin_inflate);
+        }
+
+        // vent: pressure > setpoint + deadband
+        else {
+            MAP_GPIO_setOutputHighOnPin(valves[idx_valve].port, valves[idx_valve].pin_vent);
+            MAP_GPIO_setOutputLowOnPin(valves[idx_valve].port, valves[idx_valve].pin_inflate);
+        }
+
+
     }
 //    MAP_GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN5); //DEBUG
 }
