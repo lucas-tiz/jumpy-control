099b07f66a7b5050593428421085e267cb831e48
Lucas Tiziani <lucas.tiziani@gatech.edu>
2021-01-12 16:38:31 -0500

    rewritten for trajectory write to msp

--- control_jumpy/scripts/pres_control.py
+++ control_jumpy/scripts/pres_control.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import rospy
+from mcu_uart_jumpy.msg import PresTraj
 from std_msgs.msg import Time
 from std_msgs.msg import Float32MultiArray
 from std_msgs.msg import MultiArrayDimension
 import numpy as np
+
 
 # pressure sequence [time, p1 KR, p2 HR, p3 HL, p4 KL] (s, psi)
 p1 = 45.0
@@ -43,13 +45,20 @@ p3 = 10.0
 # 			[05.112, -1.0, -1.0, -1.0, -1.0], # seal knees
 # 			[05.7,   -1.0, -1.0, -1.0, -1.0]] # end
 # 1.4:
-pres_seq = [[00.000, -1.0, -1.0, -1.0, -1.0], # seal
-			[05.000, -1.0,   p1,   p1, -1.0], # inflate hips
-			[05.018,   p1,   p1,   p1,   p1], # inflate knees
-			[05.124,   p1, -1.0, -1.0,   p1], # seal hips
-			[05.134, -1.0, -1.0, -1.0, -1.0], # seal knees
-			[05.7,   -1.0, -1.0, -1.0, -1.0]] # end
+# pres_seq = [[00.000, -1.0, -1.0, -1.0, -1.0], # seal
+# 			[05.000, -1.0,   p1,   p1, -1.0], # inflate hips
+# 			[05.018,   p1,   p1,   p1,   p1], # inflate knees
+# 			[05.124,   p1, -1.0, -1.0,   p1], # seal hips
+# 			[05.134, -1.0, -1.0, -1.0, -1.0], # seal knees
+# 			[05.7,   -1.0, -1.0, -1.0, -1.0]] # end
 
+test_pres_seq = [[00.000,  p1,   -1.0, -1.0,   p1], # seal
+			     [00.5,    p1,     p2,   p2,   p1],
+				 [00.6,   0.0,    0.0,  0.0,  0.0],
+				 [01.2,  -1.0,   -1.0, -1.0, -1.0]]
+
+vent_seq = 	 [[0.00, 00.0, 00.0, 00.0, 00.0], # vent all valves
+			  [3.0, -1.0, -1.0, -1.0, -1.0]] # seal all valves
 
 # global variables
 num_valves = 4 
@@ -58,76 +67,56 @@ num_valves = 4
 class Controller:
 	''' perform preset actuator pressure timing sequence '''
 	def __init__(self, publisher_topic):
-		# set up message
+		# set up message & publisher
 		self.msg_tx = Float32MultiArray() # create message 
-		self.msg_tx.data = np.zeros(num_valves+1, dtype=np.float32) # initialize with extra int for msg type
+		self.msg_tx.data = np.zeros(num_valves+2, dtype=np.float32) # initialize for max message length
 		self.msg_tx.layout.dim.append(MultiArrayDimension()) # add dimension to message
-		self.msg_tx.layout.dim[0].size = num_valves+1 # extra int for message type
-
-		# set up publisher and subscriber
 		self.pub_tx = rospy.Publisher(publisher_topic, Float32MultiArray, queue_size=10) # publisher of data to transmit to MCU
-		self.pub_test = rospy.Publisher('test_start', Time, queue_size=1)
-
-		self.msg_pres_des = Float32MultiArray()
-		self.msg_pres_des.data = np.zeros(num_valves, dtype=np.float32)
-		self.pub_pres_des = rospy.Publisher('pres_des', Float32MultiArray, queue_size=10)
-
-		# self.sub_tx = rospy.Subscriber(subscriber_topic, Float32MultiArray, queue_size=1, callback=self.fsrProcess) # subscriber to MCU data
 		rospy.sleep(2) # required in order to not miss initial published data
 
 
-	def runPresSequence(self, pres_seq):
+	def runPresSequence(self, pres_seq, data_dump=True):
 		''' run pneumatic actuator pressure sequence '''
-		msg_test = Time()
-		t_start = rospy.get_time() # start time of pressure sequence
-		msg_test.data = rospy.get_rostime()
-		self.pub_test.publish(msg_test)
+		# write valve time sequence to MCU
+		print('  updating valve sequence.')
+		self.msg_tx.layout.dim[0].size = num_valves+2 # set array size
+		for idx_pres_seq in range(len(pres_seq)): # loop through valve sequence list
+			self.msg_tx.data[0] = (idx_pres_seq << 8) | 3 # specify valve sequence update (LSByte) & sequence index (2nd LSByte)
+			self.msg_tx.data[1:(num_valves+2)] = pres_seq[idx_pres_seq] # add sequence data to message
+			self.pub_tx.publish(self.msg_tx) # publish message
+		rospy.sleep(0.1)
 
-		# signal LED test start
-		self.msg_tx.data[0] = 2
-		self.msg_tx.data[1] = 1
-		self.pub_tx.publish(self.msg_tx)
+		# start valve time sequence
+		self.msg_tx.layout.dim[0].size = 1 # set array size
+		self.msg_tx.data[0] = 4 # specify this is a valve sequence start 
+		self.pub_tx.publish(self.msg_tx) # publish message
+		print('  started valve time sequence.')
+		t_start = rospy.get_time() # start time of valve sequence
+		# while (rospy.get_time() - t_start) < pres_seq[-1][0]*1.5: continue # allow time for sequence to run
+		rospy.sleep(pres_seq[-1][0]*1.1) # allow time for sequence to run
 
-		self.msg_tx.data[0] = 0 # specify this is a pressure setpoint message type
-		ind_pres_seq = 0 # initialize pressure sequence index
-		while ind_pres_seq < len(pres_seq) and not rospy.is_shutdown(): # go through pressure sequence list
-			t = rospy.get_time() - t_start # get elapsed time
-			if t >= pres_seq[ind_pres_seq][0]: # if at next time in sequence
-				self.pub_pres_des.publish(self.msg_pres_des) # publish previous desired pressure
-
-				self.msg_tx.data[1:(num_valves+1)] = pres_seq[ind_pres_seq][1:(num_valves+1)] # add pressures to message
-				self.pub_tx.publish(self.msg_tx) # publish message
-				
-				self.msg_pres_des.data[:] = pres_seq[ind_pres_seq][1:(num_valves+1)] # update message
-				self.pub_pres_des.publish(self.msg_pres_des) # publish latest desired pressure
-
-				print '  pressure setpoint updated:', pres_seq[ind_pres_seq][1:(num_valves+1)], 'at time {0:.2f}'.format(t) 
-				ind_pres_seq = ind_pres_seq + 1 # increment pressure sequence index
-		if ind_pres_seq == len(pres_seq):
-
-			# signal LED test end
-			self.msg_tx.data[0] = 2
-			self.msg_tx.data[1] = 0
-			self.pub_tx.publish(self.msg_tx)
-
-			print('finished pressure sequence')
+		# ask for data back
+		if data_dump:
+			self.msg_tx.layout.dim[0].size = 1 # set array size
+			self.msg_tx.data[0] = 5 # specify this is a trajectory data dump
+			self.pub_tx.publish(self.msg_tx) # publish message
+			print('  started data dump\n')
+			rospy.sleep(pres_seq[-1][0]*2.5) # allow time for dump/plot
 
 
 if __name__ == '__main__':  
 	rospy.init_node('pres_control', anonymous=True) # initialize ROS node
 	controller = Controller('mcu_tx') # create controller
 
+	# def shutdown(): # set up venting on shutdown
+	# 	print('shutdown - venting:')
+	# 	controller.runPresSequence(vent_seq, data_dump=True)
+	# rospy.on_shutdown(shutdown)
 
+	rospy.set_param('/file_prefix', rospy.get_param('~file_prefix'))
 
-	def shutdown(): # set up venting on shutdown
-		print('shutdown: venting')
-		rospy.sleep(0.1)
-		vent_seq = 	 [[0.00, 00.0, 00.0, 00.0, 00.0], # vent all valves
-			  		  [2.00, -1.0, -1.0, -1.0, -1.0]] # seal all valves
-		controller.runPresSequence(vent_seq)
-		rospy.sleep(2)
-		print('vented!')
-	rospy.on_shutdown(shutdown)
+	controller.runPresSequence(test_pres_seq) # run pressure sequence
+	controller.runPresSequence(vent_seq, data_dump=False) # run pressure sequence
+	print('done')
 
-	print('running pressure sequence')
-	controller.runPresSequence(pres_seq) # run pressure sequence
+	rospy.spin()
